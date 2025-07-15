dafc642133551880e8308e36d37c28c6671cb27d
PDineshMurugan <dineshmurugan518@gmail.com>
2025-07-15 13:46:55 +0530

    Initial commit - portfolio project

---
+++ reload.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    document.getElementById('loading').style.display = 'none'; // Hide the loader
+    document.getElementById('content').style.display = 'block'; // Show the content
+};
