dbacf9838d1075033f2ed8b98bd7135e287dc87f
PDineshMurugan <dineshmurugan518@gmail.com>
2025-07-16 19:54:41 +0530

    site update

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// service-worker.js
+const CACHE_NAME = "offline-cache";
+const OFFLINE_URL = "offline.html";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([OFFLINE_URL]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+  }
+});
